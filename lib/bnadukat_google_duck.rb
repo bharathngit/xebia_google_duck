8b8f463a9772b2b6ab0096955a933631e5853750
Bharath <bharath@Bharaths-Mac-mini.local>
2021-02-04 17:22:16 -0800

    first commit

---
+++ lib/bnadukat_google_duck.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bnadukat_google_duck/version"
+
+module BnadukatGoogleDuck
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/bnadukat_google_duck_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe BnadukatGoogleDuck do
+  it "has a version number" do
+    expect(BnadukatGoogleDuck::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
